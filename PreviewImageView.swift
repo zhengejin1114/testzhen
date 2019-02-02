ad1dd68a8d8195f2be83869fc2f183a1ab40615e
ZhenGe Jin <zhengejin1114@outlook.com>
2019-02-02 07:57:45 +0300

    sample

---
+++ PreviewImageView.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewImageView.swift
+//  coloring
+//
+//  Created by Admin on 4/14/16.
+//  Copyright © 2016 Yuki. All rights reserved.
+//
+
+import UIKit
+
+class PreviewImageView: UIView
+{
+
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var shadowImageView: UIImageView!
+
+}
